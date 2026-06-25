db8dc3b2d56ae762c6ba75136890919aa225675a
jan-ajata <a.badykov@innopolis.university>
2026-06-25 15:49:53 +0300

    Updated frontend and backend for new TP

--- src/cnss/app/schemes/packet.py
+++ src/cnss/app/schemes/packet.py
@@ -3,8 +3,12 @@ from pydantic import BaseModel
 
 class Packet(BaseModel):
     timestamp: str
-    total_packets: int
-    total_bytes: int
+    total_packets: int = 0
+    total_bytes: int = 0
+    incoming_packets: int = 0
+    outgoing_packets: int = 0
+    incoming_bytes: int = 0
+    outgoing_bytes: int = 0
     packets_per_second: float
     bytes_per_second: float
     tcp_packets: int
